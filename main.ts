522fb0cf4d3a8c8292546823d9f65b95a60e31fe
oscarhornidge <79571606+oscarhornidge@users.noreply.github.com>
2021-05-31 12:52:37 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,20 @@
+input.onGesture(Gesture.TiltRight, function () {
+    step += 1
+    basic.showNumber(step)
+})
+input.onGesture(Gesture.LogoUp, function () {
+    step += 1
+    basic.showNumber(step)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    step += 1
+    basic.showNumber(step)
+})
 input.onGesture(Gesture.Shake, function () {
+    step += 1
+    basic.showNumber(step)
+})
+input.onGesture(Gesture.LogoDown, function () {
     step += 1
     basic.showNumber(step)
 })
